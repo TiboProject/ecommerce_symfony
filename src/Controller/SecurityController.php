37ad3e51d6e5746997103458ba18a81cea10557f
Thibaut <thibaut.coquelet@gmail.com>
2023-06-07 19:02:32 +0200

    Fix Routing for login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,13 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/login', name: 'app_security')]
+    #[Route('/login', name: 'app_security_login')]
     public function login(): Response
     {
         return $this->render('security/login.html.twig');
     }
 
-    #[Route('/signup', name: 'app_security')]
+    #[Route('/signup', name: 'app_security_signup')]
     public function signUp(): Response
     {
         return $this->render('security/signUp.html.twig');
