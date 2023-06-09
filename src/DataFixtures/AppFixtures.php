215a0965d72b385d7e2a90112d81a2b1e6d092dc
Max <max94220@hotmail.fr>
2023-06-09 22:37:05 +0200

    Display Product

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -53,6 +53,90 @@ class AppFixtures extends Fixture
             $manager->persist($product);
         }
 
+        for ($i=0;$i<4;$i++)
+        {
+        $equipes=array('Lyon Domicile','Lyon Extérieur','Paris Domicile Mbappe','Paris Domicile');
+        $image=array('https://i.goopics.net/hatckb.jpg','https://i.goopics.net/p84p1l.jpg','https://i.goopics.net/zabwn8.jpg','https://i.goopics.net/vihwm6.jpg');
+        $product= new Product();
+        $product->setName('Ensemble - '.$equipes[$i])
+            ->setCategory('Ensemble')
+            ->setPrice("90")
+            ->setPicture($image[$i]);
+    
+            $manager->persist($product);
+        }
+
+        for ($i=0;$i<4;$i++)
+        {
+        $equipeM=array('Japon Domicile','Réal Madrid Domicile', 'Lyon Extérieur', 'Paris Domicile');
+        $imageM=array('https://i.goopics.net/wl46vh.jpg','https://i.goopics.net/6qwabq.jpg','https://i.goopics.net/c82q6t.jpg','https://i.goopics.net/bqyffd.jpg');
+        $product= new Product();
+        $product->setName('Maillot - '.$equipeM[$i])
+            ->setCategory('Maillot')
+            ->setPrice("50")
+            ->setPicture($imageM[$i]);
+    
+            $manager->persist($product);
+
+        }
+
+        for ($i=0;$i<4;$i++)
+        {
+        $Marque=array('Prédator X1','Prédator X2', 'Prédator X3', 'Prédator X4');
+        $imageC=array('https://i.goopics.net/8l04rj.jpg','https://i.goopics.net/f1j3ce.jpg','https://i.goopics.net/prajh2.jpg','https://i.goopics.net/lcxogf.jpg');
+        $product= new Product();
+        $product->setName('Crampons - '.$Marque[$i])
+            ->setCategory('Crampons')
+            ->setPrice("30")
+            ->setPicture($imageC[$i]);
+    
+            $manager->persist($product);
+
+        }
+
+        for ($i=0;$i<4;$i++)
+        {
+        $Marques=array('Lyon 2002','Lyon 2003', 'Lyon 2007', 'Lyon 2015');
+        $imageB=array('https://i.goopics.net/t2294y.jpg','https://i.goopics.net/edy5kh.jpg','https://i.goopics.net/538jte.jpg','https://i.goopics.net/wq4nye.jpg');
+        $product= new Product();
+        $product->setName('Ballon - '.$Marques[$i])
+            ->setCategory('Ballon')
+            ->setPrice("10")
+            ->setPicture($imageB[$i]);
+    
+            $manager->persist($product);
+
+        }
+
+        for ($i=0;$i<4;$i++)
+        {
+        $MarquesG=array('Lyon 2004','Lyon Fourrure', 'Adidas 2007', 'Adidas 2015');
+        $imageG=array('https://i.goopics.net/noz4i2.jpg','https://i.goopics.net/ngnlej.jpg','https://i.goopics.net/23ujbl.jpg','https://i.goopics.net/l6121r.jpg');
+        $product= new Product();
+        $product->setName('Gants - '.$MarquesG[$i])
+            ->setCategory('Gants')
+            ->setPrice("15")
+            ->setPicture($imageG[$i]);
+    
+            $manager->persist($product);
+
+        }
+
+        for ($i=0;$i<4;$i++)
+        {
+        $Goodies=array('Sac Paris','Mug Lyon', 'Echarpe Paris', 'Figurine Marquinhos');
+        $imageGoo=array('https://i.goopics.net/t9o0a9.jpg','https://i.goopics.net/p2n2l1.jpg','https://i.goopics.net/2ijbxd.jpg','https://i.goopics.net/l9wqyh.jpg');
+        $product= new Product();
+        $product->setName('Goodies - '.$Goodies[$i])
+            ->setCategory('Goodies')
+            ->setPrice("8")
+            ->setPicture($imageGoo[$i]);
+    
+            $manager->persist($product);
+
+        }
+
+
 
         for ($i=1;$i<=5;$i++)
         {
